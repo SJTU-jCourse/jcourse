c3804de50fafc50663f2854fb416e1709a60b2ca
dujiajun <jiajun_du@outlook.com>
2021-06-03 12:49:49 +0800

    update contact email

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,7 @@ const NotFound = () => {
     <Result
       status="404"
       title="哎呀抱歉，你要找的页面找不到了"
-      subTitle="如果这是网站的bug，或者要找的页面你很重要，请邮件联系我们：jcourse@sjtu.plus"
+      subTitle="如果这是网站的bug，或者要找的页面你很重要，请邮件联系我们：course@sjtu.plus"
     />
   );
 };
