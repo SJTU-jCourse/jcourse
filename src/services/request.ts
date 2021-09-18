3a68c0ba2acbe6e95dca1fa2a122469391b92ecd
dujiajun <jiajun_du@outlook.com>
2021-09-18 18:23:36 +0800

    show 429 error message

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,9 +1,11 @@
+import { message } from 'antd';
 import axios from 'axios';
 import { history } from 'umi';
 
 const errorHandler = function (error: { response: { status: number } }) {
   if (error.response?.status == 403) history.push('/login');
   if (error.response?.status == 404) history.push('/404');
+  if (error.response?.status == 429) message.error('操作太频繁，请稍后再试！');
   throw error;
 };
 
