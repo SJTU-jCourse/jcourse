49960065f5ebb62fccde4ed1e64832d8030c2c9d
Du Jiajun <jiajun_du@outlook.com>
2023-01-03 20:41:38 +0800

    feat: update year

--- src/components/layouts.tsx
+++ src/components/layouts.tsx
@@ -35,7 +35,7 @@ export const BasicLayout = ({ children }: React.PropsWithChildren<{}>) => {
           <Link href="/faq">常见问题</Link>
           <Link href="/report">反馈</Link>
         </Space>
-        <div>©2022 SJTU选课社区</div>
+        <div>©2023 SJTU选课社区</div>
       </Footer>
     </Layout>
   );
